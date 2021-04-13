1a072c030584900002798f44485e8308bc7dd65d
drkwdck <reshetnikov.e.a@mail.ru>
2021-04-13 21:53:24 +0300

    добавлены замеры времени

--- FastFourierTransform/main.cpp
+++ FastFourierTransform/main.cpp
@@ -1,66 +1,71 @@
 #include <bits/stdc++.h>
 #include <iostream>
 #include <fstream>
+#include <time.h>
 #include "Utils.h"
 #include "DiscreteFourierTransform.h"
 #include "FastFourierTransform.h"
 
-
-
 int main()
 {
     // Задание №3
-    const int signalPoints = 16;
+    const int signalPoints = 1024;
     char* singalFilePath = "./data.txt";
     GenerateSignalIntoFile(singalFilePath, signalPoints, 0);
-
     auto signal = ReadSignalFromFile(singalFilePath);
     auto restored_signal = DFT(signal);
     std::cout << "Разница между X и ОДПФ(ДПФ(X)) MAE = " << MAE(signal, iDFT(restored_signal)) << std::endl;
 
-    auto singalCopy1 = std::vector<std::complex<double>>(signal.size(), 0);
-    for (size_t i = 0; i < singalCopy1.size(); ++i)
-    {
-        singalCopy1[i] = std::complex<double>(signal[i].real(), signal[i].imag());
-    }
-
+    GenerateSignalIntoFile(singalFilePath, signalPoints, 0);
+    auto signalCopy = ReadSignalFromFile(singalFilePath);
     FastFourierTransform(signal);
     std::cout << "Разница между ДПФ(X) и БПФ(X) MAE = "<< MAE(signal, restored_signal)  << std::endl;
-    FastFourierTransform(signal, true);
 
-    std::cout << "Разница между X и ОБПФ(БПФ(X)) MAE = " << MAE(singalCopy1, signal) << std::endl;
+    // для matlab
+    std::ofstream fileStream("fft.txt");
+    std::ofstream fileStreamX("x_fft.txt");
+    for (size_t i = 0; i < signal.size(); ++i)
+    {
+        fileStream << "complex(" << restored_signal[i].real() << ", " << restored_signal[i].imag() << ") ";
+        fileStreamX << "complex(" << signalCopy[i].real() << ", " << signalCopy[i].imag() << ") ";
+    }
+    fileStream.close();
+    fileStreamX.close();
+
+    FastFourierTransform(signal, true);
+    std::cout << "Разница между X и ОБПФ(БПФ(X)) MAE = " << MAE(signalCopy, signal) << std::endl;
     signal.clear();
-    singalCopy1.clear();
+    signalCopy.clear();
     restored_signal.clear();
 
+
+
     // Задание №4
-    // time_t start, end;
-    for (int i = 10; i <= 1000; i *= 10)
+    clock_t start, end;
+    
+    // Замеры времени для ДПФ
+    fileStream  = std::ofstream("./time_dft.txt");
+    for (int i = 256; i < 256 * 32; i *= 2)
     {
         auto x = GenerateSample(i, 0);
-        clock_t tStart = clock();
+        start = clock();
         DFT(x);
-        std::cout << std::fixed << double(clock() - tStart)   << " ";
-        x.clear();
+        end = clock();
+        fileStream << double(end - start) / CLOCKS_PER_SEC << " ";
     }
+    fileStream.close();
 
-    std::cout << std::endl;
-
-    // std::vector<double> timesDelayFFT;
-    // for (int i = 100; i < 1000; i *= 10)
-    // {
-    //     auto x = GenerateSample(i, 0);
-    //     time(&start);
-    //     DFT(x);
-    //     time(&end);
-    //     timesDelayFFT.push_back(end - start);
-    // }
-    // fileStream  = std::ofstream("./time_fft.txt");
-    // for (size_t i = 0; i < timesDelayFFT.size(); ++i)
-    // {
-    //     fileStream << timesDelayFFT[i] << '\n';
-    // }
-    // fileStream.close();
+    // Замеры для БПФ
+    fileStream  = std::ofstream("./time_fft.txt");
+    for (int i = 256; i < 256 * 32; i *= 2)
+    {
+        auto x = GenerateSample(i, 0);
+        start = clock();
+        FastFourierTransform(x);
+        end = clock();
+        fileStream << double(end - start) / CLOCKS_PER_SEC << " ";
+    }
+    fileStream.close();
 
     return 0;
 }
